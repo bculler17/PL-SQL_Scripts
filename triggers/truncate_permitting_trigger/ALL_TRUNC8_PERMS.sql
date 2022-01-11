ae83b7e4b89d339752f179b6199f1d6f5bbd64b5
Beth Culler <49499193+bculler17@users.noreply.github.com>
2022-01-11 13:28:13 -0600

    Delete triggers/truncate_permitting_trigger directory

--- triggers/truncate_permitting_trigger/ALL_TRUNC8_PERMS.sql
+++
@@ -1,8 +0,0 @@
-create table ALL_TRUNC8_PERMS (
-  username VARCHAR2(25),
-  ownername VARCHAR2(25),
-  tablename VARCHAR2(45)
-);
--- Where username = the user to audit/manage with the "DROP ANY" system privilege,
--- ownername = the owner of the table that the user is permitted to truncate
--- tablename = the name of the table that the user is permitted to truncate
